5ca16c65c83b9454806780fd944c53126f4c4117
Cyan <anhtai05102002@gmail.com>
2023-04-14 16:02:38 +0700

    NavBar

--- WEB/Controllers/HomeController.cs
+++ WEB/Controllers/HomeController.cs
@@ -7,18 +7,19 @@ namespace WEB.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Search()
         {
             return View();
         }
